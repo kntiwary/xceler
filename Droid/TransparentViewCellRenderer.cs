b06f685f2a73073ca79af7ff2a6dd242278b52dd
Kamta Nath Tiwary <kamta@Kamtas-MacBook-Air.local>
2018-01-30 01:42:49 +0530

    json demo implement

---
+++ Droid/TransparentViewCellRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.Content;
+using Android.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using xceler.customControls;
+using xceler.Droid;
+
+[assembly: ExportRenderer(typeof(TransparentViewCell), typeof(TransparentViewCellRenderer))]
+namespace xceler.Droid
+{
+    public class TransparentViewCellRenderer : ViewCellRenderer
+    {
+        protected override Android.Views.View GetCellCore(Cell item, Android.Views.View convertView, ViewGroup parent, Context context)
+        {
+            var cell = base.GetCellCore(item, convertView, parent, context);
+
+            var listView = parent as Android.Widget.ListView;
+
+            if (listView != null)
+            {
+                cell.SetBackgroundColor(Android.Graphics.Color.ParseColor("#f2eaea"));
+                // Disable native cell selection color style - set as *Transparent*
+                listView.SetSelector(Android.Resource.Color.White);
+
+
+                listView.CacheColorHint = Android.Graphics.Color.Transparent;
+            }
+
+            return cell;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ iOS/TransparentViewCellRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using xceler.customControls;
+using xceler.iOS;
+
+[assembly: ExportRenderer(typeof(TransparentViewCell), typeof(TransparentViewCellRenderer))]
+namespace xceler.iOS
+{
+    public class TransparentViewCellRenderer : ViewCellRenderer
+    {
+        
+
+        public override UIKit.UITableViewCell GetCell(Cell item, UIKit.UITableViewCell reusableCell, UIKit.UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            if (cell != null)
+            {
+                cell.SelectionStyle = UIKit.UITableViewCellSelectionStyle.None;
+            }
+
+            return base.GetCell(item, reusableCell, tv);
+
+        }
+    }
+}
